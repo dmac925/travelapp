07bbab1362ffb1be2a96b3746c6b808b049a9a99
Andrew Gove <andrewgove@macbook-air-8.home>
2023-11-05 18:15:31 +0000

    nov5 v2

--- travelapp/src/config.js
+++ travelapp/src/config.js
@@ -1,5 +1,1 @@
-export const URL =
-  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
-    ? "http://localhost:4000"
-    : "https://tan-victorious-dibbler.cyclic.app/";
-
+export const URL = "https://travelapp-23y6.onrender.com/"
